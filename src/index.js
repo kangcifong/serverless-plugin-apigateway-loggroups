e9189d1a721f946651801ca8afbfddbabab7a095
Kang Cifong <kang_c@worksap.co.jp>
2020-05-13 20:02:42 +0900

    Fix typo and add check

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
+'use strict';
+
 class CustomizeLogGroupProperties {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.provider = this.serverless.getProvider('aws');
-
+    this.config = serverless.service.custom['serverless-plugin-apigateway-loggroups'];
+    if (!this.config)
+      throw new Error('serverless-plugin-apigateway-loggroups should be added in cutom field');
     this.hooks = {
       'before:aws:package:finalize:mergeCustomProviderResources': this.customize.bind(this)
     };
   }
   customize(){
     const template = this.serverless.service.provider.compiledCloudFormationTemplate;
-    template.Resource['ApiGatewayLogGroup'].Properties.LogGroupName = this.configuration['log-group-name'];
+    template.Resources['ApiGatewayLogGroup'].Properties.LogGroupName = this.config['logGroupName'];
     /*
    https://github.com/serverless/serverless/blob/3910df1ba6a8b39367ce8d51adb90216251be2ba/lib/plugins/aws/lib/naming.js#L310
     */
   }
 }
 
-module.expors = CustomizeLogGroupProperties;
+module.exports = CustomizeLogGroupProperties;
